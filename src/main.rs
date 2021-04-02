6e56fe0987919e73d02c413af8f28f0109711b6e
janaSunrise <warriordefenderz@gmail.com>
2021-04-02 21:41:45 +0530

    Fix a warning.

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, ready: Ready) {
+    async fn ready(&self, _ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
         if let Some(shard) = ready.shard {
