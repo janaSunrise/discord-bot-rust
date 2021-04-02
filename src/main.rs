2884fb0e0ca79aa7a9f7e17ef9f08333b9049c65
janaSunrise <warriordefenderz@gmail.com>
2021-04-02 22:10:22 +0530

    Organize things, and add one more command.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{ env, sync::Arc, time::Duration};
+use std::{ collections::{ HashSet }, env, sync::Arc, time::Duration};
 
 use serenity::{
     async_trait,
@@ -12,15 +12,21 @@ use serenity::{
     },
     framework::{
         standard::{
+            Args,
+            CommandGroup,
             CommandResult,
             StandardFramework,
-            macros::{ command, group }
+            HelpOptions,
+            help_commands,
+            macros::{ command, group, help }
         }
     },
     model::{
-        channel::{ Message },
-        gateway::{ Ready },
+        channel::Message,
+        gateway::Ready,
+        id::UserId,
     },
+    http::Http,
     prelude::*
 };
 use tokio::time::sleep;
@@ -32,9 +38,32 @@ impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
 }
 
+#[help]
+#[individual_command_tip =
+"Hello! こんにちは！Hola! Bonjour! 您好! 안녕하세요~\n\n\
+If you want more information about a specific command, just pass the command as argument."]
+#[command_not_found_text = "Could not find: `{}`."]
+#[max_levenshtein_distance(3)]
+#[indention_prefix = "+"]
+#[lacking_permissions = "Hide"]
+#[lacking_role = "Nothing"]
+#[wrong_channel = "Strike"]
+async fn my_help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+
+    Ok(())
+}
+
 // Commands implementation
 #[group]
-#[commands(ping)]
+#[commands(ping, latency)]
 struct General;
 
 // Handler impl
@@ -42,7 +71,7 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
         if let Some(shard) = ready.shard {
@@ -59,18 +88,52 @@ impl EventHandler for Handler {
 // Main section
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~"))
-        .group(&GENERAL_GROUP);
-
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
 
+    // Fetch owners
+    let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    // Define the framework
+    let framework = StandardFramework::new()
+        .configure(|c| c
+                    .with_whitespace(true)
+                    .on_mention(Some(bot_id))
+                    .prefix("~")
+                    .delimiters(vec![", ", ","])
+                    .owners(owners)
+        )
+        .help(&MY_HELP)
+        .group(&GENERAL_GROUP);
+
+    // Define the client
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
 
     // Shard management
     let manager = client.shard_manager.clone();
@@ -103,6 +166,36 @@ async fn main() {
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, format!("Pong! Shard: {}", ctx.shard_id)).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn latency(ctx: &Context, msg: &Message) -> CommandResult {
+    let data = ctx.data.read().await;
+
+    let shard_manager = match data.get::<ShardManagerContainer>() {
+        Some(v) => v,
+        None => {
+            msg.reply(ctx, "There was a problem getting the shard manager").await?;
+
+            return Ok(());
+        },
+    };
+
+    let manager = shard_manager.lock().await;
+    let runners = manager.runners.lock().await;
+
+    let runner = match runners.get(&ShardId(ctx.shard_id)) {
+        Some(runner) => runner,
+        None => {
+            msg.reply(ctx,  "No shard found").await?;
+
+            return Ok(());
+        },
+    };
+
+    msg.reply(ctx, &format!("The shard latency is {:?}", runner.latency)).await?;
 
     Ok(())
 }
